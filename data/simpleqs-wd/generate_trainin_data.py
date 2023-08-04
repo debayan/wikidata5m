f29c2fad0419c5f2a45b8a89325fce982fa52f6b
Debayan Banerjee <debayanin@gmail.com>
2023-08-04 11:37:35 +0000

    Also add s-p

--- data/simpleqs-wd/generate_trainin_data.py
+++ data/simpleqs-wd/generate_trainin_data.py
@@ -2,7 +2,7 @@ import sys,os,json,glob,random
 
 
 prop_labels = {}
-with open('../data/wikidata5m_relation.txt') as infile:
+with open('../wikidata5m_relation.txt') as infile:
     for line in infile:
         line = line.strip()
         items = line.split("\t")
@@ -12,27 +12,26 @@ with open('../data/wikidata5m_relation.txt') as infile:
 
 
 master_dict = {}
-for file_path in glob.glob("newids/*.json"):
-    if 'sample.json' in file_path:
-        continue
+for file_path in glob.glob("../../scripts/newids/*.json"):
     print("Reading ",file_path)
     d = json.loads(open(file_path).read())
     master_dict.update(d)
     print(len(master_dict))
 
-f = open('../data/pre-train-data.txt','w')
-with open('../data/wikidata_valid_only1.txt') as infile:
-    count = 0
-    for line in infile:
-        count += 1
-        try:
-            s,p,o = line.strip().split("\t")
-            triple = master_dict[s]['label']+' : '+' : '.join(master_dict[s]['newid'].split(':'))+' --  '+p+' : '+random.choice(prop_labels[p])+' -- '+master_dict[o]['label']+' : '+' : '.join(master_dict[o]['newid'].split(':'))
-            if count%10000 == 0:
-                print(triple)
-                print(count)
-            f.write(triple+'\n')
-        except Exception as err:
-            pass
-f.close()
-
+f = open(sys.argv[1]).readlines()
+fw = open(sys.argv[2],'w')
+for line in f:
+    line = line.strip()
+    s,p,o,q = line.split('\t')
+    if s not in master_dict or o not in master_dict or p not in prop_labels:
+        continue
+    print(s,p,o,q)
+    print(master_dict[s])
+    chain = master_dict[s]['label']+':'+master_dict[s]['newid'] + ' -- ' + p+':'+prop_labels[p][0] + ' -- '+master_dict[o]['label']+':'+master_dict[o]['newid']
+    sp = master_dict[s]['label']+':'+master_dict[s]['newid'] + ' -- ' + p+':'+prop_labels[p][0]
+    sp = sp.replace(':',' : ')
+    answer = master_dict[o]['label']+' : '+master_dict[o]['newid']
+    chain = chain.replace(':',' : ')
+    jsonline = {'question':q, 'spo':[s,p,o], 'chain':chain, 'answer':answer, 'sp':sp}
+    fw.write(json.dumps(jsonline)+'\n')
+fw.close()
